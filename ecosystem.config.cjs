74251b81acdfb212619e3b9da00fdb54828376da
Muhammad Saad Tariq <159819654+saadtariq57@users.noreply.github.com>
2026-05-25 15:58:49 +0500

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,21 +1,29 @@
+require("dotenv").config({ path: "./.env" });
+
+const PORT = process.env.PORT || "3000";
+const WEBSOCKET_PORT = process.env.WEBSOCKET_PORT || "3002";
+
 module.exports = {
   apps: [
     {
       name: "kr-next-pos",
-      script: "npm",
-      args: "run start",
+      script: "node_modules/next/dist/bin/next",
+      args: `start -p ${PORT}`,
       cwd: "./",
+      env_file: "./.env",
       env: {
         NODE_ENV: "production",
+        PORT,
       },
     },
     {
       name: "kr-websocket",
-      script: "npm",
-      args: "run start:ws",
+      script: "websocket/server.js",
       cwd: "./",
+      env_file: "./.env",
       env: {
         NODE_ENV: "production",
+        WEBSOCKET_PORT,
       },
     },
   ],
